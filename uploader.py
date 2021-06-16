a2487807916f345533cba4a31e9ba0dd3f7e34df
Spirit-78 <48299218+Spirit-78@users.noreply.github.com>
2021-06-16 23:15:47 +0200

    minor functionality

--- uploader.py
+++ uploader.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
 import sys
 from cryptography.fernet import Fernet
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
+from Crypto.Hash import SHA
+import datetime
 
 
 #cert = x509.load_pem_x509_certificate(pem_data, default_backend())
@@ -12,19 +14,26 @@ from cryptography.hazmat.backends import default_backend
 app = Flask(__name__)
 
 class EncService:
-    def __init__(self, cert_id):
-        self.key = ''
-        self.cert_id = cert_id
+    def __init__(self, cert_hash):
+        self.key = Fernet.generate_key()
+        self.cert_hash = cert_hash
 
     def write_key(self):
         with open('keys.key', 'wb') as keys_file:
-            keys_file.write(self.cert_id + ":" + self.key.decode('utf-8'))
+            keys_file.write('\n' + self.cert_hash + ":" + self.key.decode('latin1'))
 
-    def generate_key(self):
-        self.key = Fernet.generate_key()
+    def check_cert(self):
+        with open('keys/keys.key', 'r') as key_file:
+            for line in key_file.readlines():
+                if line.split(':')[0] == self.cert_hash:
+                    self.key = line.split(':')[1].encode('utf-8')
+                    return
+        self.write_key()
 
-    def  check_cert(self):
-        pass
+
+def check_date(cert):
+    return cert.not_valid_before.timestamp() < datetime.datetime.now().timestamp() < cert.not_valid_after.timestamp()
+
 
 @app.route('/')
 def index():
@@ -36,12 +45,20 @@ def upload_file():
     if request.method == 'POST':
         f = request.files['certificate']
         f.save("certificates/" + secure_filename(f.filename))
-        print('This is standard output', file=sys.stdout)
-        # '<p>file uploaded successfully<p>'
-        """check here for validity and call class object for key enc/dec"""
-        return render_template('vtora_strana.html')
-    else:
-        return '<p>something went wrong.</p>'
+        with open('certificates/' + secure_filename(f.filename), 'r') as file:
+            data = ''.join(file.readlines()).encode('utf-8')
+            cert = x509.load_pem_x509_certificate(data, default_backend())
+            print(f'This is standard output {cert.signature}', file=sys.stdout)
+            if not check_date(cert):
+                return render_template('not_valid.html')
+            handler = EncService(SHA.new(cert.signature).hexdigest())
+            handler.check_cert()
+        return redirect('/functions')
+
+
+@app.route('/functions')
+def enc_dec():
+    return render_template('vtora_strana.html')
 
 
 if __name__ == '__main__':
