d8d67dd41a6d76381969ad0dc26851fe4b2b9698
Spirit-78 <48299218+Spirit-78@users.noreply.github.com>
2021-06-24 00:55:57 +0200

    done

--- uploader.py
+++ uploader.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from werkzeug.utils import secure_filename
 import sys
 from cryptography.fernet import Fernet
@@ -7,19 +7,30 @@ from cryptography.hazmat.backends import default_backend
 from Crypto.Hash import SHA
 import datetime
 
-
 #cert = x509.load_pem_x509_certificate(pem_data, default_backend())
 # cert.serial_number
 
 app = Flask(__name__)
 
+
 class EncService:
-    def __init__(self, cert_hash):
+    def __init__(self):
         self.key = Fernet.generate_key()
+        self.cert_hash = ''
+        self.doc_name = ''
+
+    def set_cert_hash(self, cert_hash):
         self.cert_hash = cert_hash
 
+    def set_doc_name(self, doc_name):
+        self.doc_name = doc_name
+
+    def get_doc_name(self):
+        return self.doc_name
+
     def write_key(self):
-        with open('keys.key', 'wb') as keys_file:
+        with open('keys/keys.key', 'a') as keys_file:
+            print('WRITING KEY: ', self.key, 'WRITING CERT_HASH: ', self.cert_hash, file=sys.stdout)
             keys_file.write('\n' + self.cert_hash + ":" + self.key.decode('latin1'))
 
     def check_cert(self):
@@ -29,6 +40,28 @@ class EncService:
                     self.key = line.split(':')[1].encode('utf-8')
                     return
         self.write_key()
+
+    def print_details(self):
+        print('KEY: ', self.key, 'CERT_HASH: ', self.cert_hash, 'DOC NAME: ', self.doc_name, file=sys.stdout)
+
+    def encrypt_file(self):
+        fernet = Fernet(self.key)
+        with open(f'documents/{self.doc_name}', 'r') as file:
+            enc_data = fernet.encrypt(''.join(file.readlines()).encode('utf-8'))
+            print('ENCRYPTING DATA: ', enc_data, file=sys.stdout)
+            with open(f'modified/client_file', 'wb') as enc_file:
+                enc_file.write(enc_data)
+
+    def decrypt_file(self):
+        fernet = Fernet(self.key)
+        with open(f'documents/{self.doc_name}', 'r') as file:
+            dec_data = fernet.decrypt(''.join(file.readlines()).encode('utf-8'))
+            print('DECRYPTING DATA: ', dec_data, file=sys.stdout)
+            with open('modified/client_file', 'wb') as dec_file:
+                dec_file.write(dec_data)
+
+
+handler = EncService()
 
 
 def check_date(cert):
@@ -48,10 +81,11 @@ def upload_file():
         with open('certificates/' + secure_filename(f.filename), 'r') as file:
             data = ''.join(file.readlines()).encode('utf-8')
             cert = x509.load_pem_x509_certificate(data, default_backend())
-            print(f'This is standard output {cert.signature}', file=sys.stdout)
+            print(f'UPLOAD FILE HEXDIGEST FROM CERT_SIGNATURE: {SHA.new(cert.signature).hexdigest()}', file=sys.stdout)
             if not check_date(cert):
                 return render_template('not_valid.html')
-            handler = EncService(SHA.new(cert.signature).hexdigest())
+            global handler
+            handler.set_cert_hash(SHA.new(cert.signature).hexdigest())
             handler.check_cert()
         return redirect('/functions')
 
@@ -59,6 +93,38 @@ def upload_file():
 @app.route('/functions')
 def enc_dec():
     return render_template('vtora_strana.html')
+
+
+@app.route('/doc_upload', methods=['POST'])
+def doc_upload():
+    print(f'BUTTON CLICKED: {request.form["action"]}', file=sys.stdout)
+    f = request.files['document']
+    f.save("documents/" + secure_filename(f.filename))
+    global handler
+    handler.set_doc_name(secure_filename(f.filename))
+    return redirect('/encrypt') if request.form['action'] == 'Encrypt' else redirect('/decrypt')
+
+
+@app.route('/encrypt')
+def encrypt():
+    global handler
+    handler.print_details()
+    handler.encrypt_file()
+    return render_template('download_file.html')
+
+
+@app.route('/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    print('filename: ', filename, file=sys.stdout)
+    return send_file('modified/client_file', as_attachment=True)
+
+
+@app.route('/decrypt')
+def decrypt():
+    global handler
+    handler.print_details()
+    handler.decrypt_file()
+    return render_template('download_file.html')
 
 
 if __name__ == '__main__':
